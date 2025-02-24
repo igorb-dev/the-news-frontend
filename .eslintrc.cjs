10f31a155092ce2e6f2722f3918de98e1a30dcde
igorb-dev <bitencourt.igor@hotmail.com>
2025-02-23 23:13:34 -0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "prettier"],
+    settings: { react: { version: "detect" } },
+  };
+  
